0409aa48ae4d5c164f95d518ae91227510d4df8c
Ribeiro <baribeiro@deloitte.pt>
2025-10-03 15:16:25 +0100

    auth w/ google almost

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+require('./services/passport');
+
+
+
 const app = express();
+require('./routes/authRoutes')(app);
 
-app.get('/', (req, res) => {
-    res.send({bye: 'buddy'});
-});
 
-const port = process.env.PORT || 10000;
-app.listen(port);
+
+
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
+
+
