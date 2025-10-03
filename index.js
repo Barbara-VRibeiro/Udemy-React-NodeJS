3f1e10f24bbcd171d9e4846f0978a51a6f4c49ad
Ribeiro <baribeiro@deloitte.pt>
2025-10-03 09:29:49 +0100

    change to see if works

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 10000 
 
 app.get('/', (req, res) => {
-    res.send({hi:'there!'});
+    res.send({bye: 'buddy'});
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const port = process.env.PORT || 10000;
+app.listen(port);
