45c1954063212fb570a82674579cbac09da89d89
Ribeiro <baribeiro@deloitte.pt>
2025-10-23 19:23:27 +0100

    99- Billing Considerations

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,22 +1,39 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { Link } from "react-router-dom";
 
 class Header extends Component {
+
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return 'Still deciding';
+            case false:
+                return <li><a href="/auth/google">Login With Google</a></li>;
+            default:
+                return <li><a href="/api/logout">Logout</a></li>;
+        }
+    }
+
+
     render() {
         return (
             <nav>
                 <div className="nav-wrapper">
-                    <a className="left brand-logo">
+                    <Link to={this.props.auth ? '/surveys' : '/'}
+                        className="left brand-logo"
+                    >
                         Node Js and React
-                    </a>
+                    </Link>
                     <ul className="right">
-                        <li>
-                            <a>Login with Google</a>
-                        </li>
+                        {this.renderContent()}
                     </ul>
                 </div>
             </nav>
         );
     }
 }
-
-export default Header;
+function mapStateToProps({ auth }) {
+    return { auth };
+}
+export default connect(mapStateToProps)(Header);
