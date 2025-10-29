22098159b74bd90c1dea9b2d5474ec4643720bd2
Ribeiro <baribeiro@deloitte.pt>
2025-10-29 14:34:50 +0000

    118- Ended Payments.

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
+const requireLogin = require('./middlewares/requireLogin');
 require('./models/user');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
-
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -20,6 +22,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
